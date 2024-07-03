d7a83c515d586392dc942e3541271229aa707f76
pilming44 <pilming44@naver.com>
2024-07-03 17:05:31 +0900

    Line 엔티티 생성

---
+++ src/main/java/subway/entity/Line.java
@@ -0,0 +1,25 @@
+package subway.entity;
+
+import javax.persistence.*;
+
+@Entity
+public class Line {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    private String color;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    private Station upStation;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    private Station downStation;
+
+    private Long distance;
+
+    protected Line() {
+    }
+}
