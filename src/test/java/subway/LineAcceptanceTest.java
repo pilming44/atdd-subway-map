5bf4fc5007ea9a39c2aba836fa76237fc84bf078
pilming44 <pilming44@naver.com>
2024-07-03 17:45:52 +0900

    지하철 노선 조회 테스트 작성

--- src/test/java/subway/LineAcceptanceTest.java
+++ src/test/java/subway/LineAcceptanceTest.java
@@ -9,7 +9,6 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.jdbc.Sql;
-import subway.entity.Station;
 
 import java.util.HashMap;
 import java.util.List;
@@ -72,8 +71,7 @@ public class LineAcceptanceTest {
                 .body(params1)
                 .contentType(MediaType.APPLICATION_JSON_VALUE)
                 .when().post("/lines")
-                .then().log().all()
-                .extract();
+                .then().log().all();
 
         Map<String, Object> params2 = new HashMap<>();
         params2.put("name", "분당선");
@@ -86,8 +84,7 @@ public class LineAcceptanceTest {
                 .body(params2)
                 .contentType(MediaType.APPLICATION_JSON_VALUE)
                 .when().post("/lines")
-                .then().log().all()
-                .extract();
+                .then().log().all();
 
         // when
         List<Map<String, Object>> response = RestAssured.given().log().all()
@@ -103,17 +100,51 @@ public class LineAcceptanceTest {
         List<Map<String, Object>> stations1 = (List<Map<String, Object>>) response.get(0).get("stations");
         assertThat(stations1.size()).isEqualTo(2);
         assertThat(stations1.get(0).get("id")).isEqualTo(1);
-        assertThat(stations1.get(0).get("name")).isEqualTo("강남역");
         assertThat(stations1.get(1).get("id")).isEqualTo(2);
-        assertThat(stations1.get(1).get("name")).isEqualTo("판교역");
 
         assertThat(response.get(1).get("name")).isEqualTo("분당선");
         assertThat(response.get(1).get("color")).isEqualTo("bg-green-600");
         List<Map<String, Object>> stations2 = (List<Map<String, Object>>) response.get(1).get("stations");
         assertThat(stations2.size()).isEqualTo(2);
         assertThat(stations2.get(0).get("id")).isEqualTo(1);
-        assertThat(stations2.get(0).get("name")).isEqualTo("강남역");
         assertThat(stations2.get(1).get("id")).isEqualTo(3);
-        assertThat(stations2.get(1).get("name")).isEqualTo("광교역");
+    }
+
+    @Test
+    @DisplayName("지하철 노선을 조회한다.")
+    void viewLine() {
+        // given
+        Map<String, Object> params1 = new HashMap<>();
+        params1.put("name", "신분당선");
+        params1.put("color", "bg-red-600");
+        params1.put("upStationId", 1);
+        params1.put("downStationId", 2);
+        params1.put("distance", 10);
+
+        long lineId = RestAssured.given().log().all()
+                .body(params1)
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .when().post("/lines")
+                .then().log().all()
+                .extract().jsonPath().getLong("id");
+
+        // when
+        ExtractableResponse<Response> response = RestAssured.given().log().all()
+                .when().get("/lines/" + lineId)
+                .then().log().all()
+                .extract();
+
+        // then
+        Long responseLineId = response.jsonPath().getLong("id");
+        String responseLineName = response.jsonPath().getString("name");
+        String responseLineColor = response.jsonPath().getString("color");
+        List<Map<String, Object>> stations = response.jsonPath().getList("stations");
+
+        assertThat(responseLineId).isEqualTo(lineId);
+        assertThat(responseLineName).isEqualTo("신분당선");
+        assertThat(responseLineColor).isEqualTo("bg-red-600");
+        assertThat(stations).hasSize(2);
+        assertThat(stations.get(0).get("id")).isEqualTo(1);
+        assertThat(stations.get(1).get("id")).isEqualTo(2);
     }
 }
